2f5edfb8c61d4f281b7828cf80002c7db95a8dda
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-05-30 18:23:11 +0100

    redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import Home from "./components/home"
-import ContactUs from "./components/contactUs"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-function App() {
+import Home from "./components/home";
+import ContactUs from "./components/contactUs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element= {<Home/>} />
-          <Route path="/home" element={<Home/>} /> 
-          <Route path="/contact" element={<ContactUs/>} />
-        </Routes>
-      </Router>
-  )
+    <Router basename="/michelle-website"> {/* <-- The key change is here! */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} /> 
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
