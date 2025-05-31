45ef3dd0c909041074f9c2d916c55d7c04d430fc
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-05-31 19:33:00 +0100

    remodified the text and also the style

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -16,7 +16,7 @@ function Team()  {
             <div className="contacts">
                 <Contact imageLink="./faith.jpg" name="Michelle Agboola" content="Managing Director and Client Experience
 I specialized in Project Management, Contract Negotiations, and Compliance for 10 years prior to purchasing vacant property.
-I help people like yourself that own land to remove the inconvenience paying to upkeep land you don’t plan on usinf. I actively buy land, collaborate with landowners, title, and other land professionals to create a seamless transition.
+I help people like yourself that own land to remove the inconvenience paying to upkeep land you don’t plan on using. I actively buy land, collaborate with landowners, title, and other land professionals to create a seamless transition.
 " />
                 <Contact imageLink="./mitch.jpg" name="Faith John" content="Marketing Manager and Contracts Assistant
 Hi, I’m Faith, the Marketing Manager and Contract Assistant here at our real estate investment company! I love bringing energy and creativity to everything I do-whether it’s crafting fun, targeted marketing campaigns or making sure our contracts are spot-on and stress-free. I’m passionate about helping our company grow by blending smart marketing with careful contract management. I thrive on teamwork, good vibes, and turning challenges into opportunities. Let’s make real estate investing exciting and successful-together!
