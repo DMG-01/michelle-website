49b9f22858efe8c4465340159e89713e37b2e9be
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-06-03 08:59:49 +0100

    changed info@magboo.co to hello@magboo.co

--- src/components/subscribe.jsx
+++ src/components/subscribe.jsx
@@ -5,7 +5,7 @@ function Subscribe() {
     return(
         <div className="subscribe">
             <div><h2>CONTACT</h2></div>
-            <div><p>The process is super fast and super simple. If you're interested or want to know more, complete this form or email us at info@magboo.co</p></div>
+            <div><p>The process is super fast and super simple. If you're interested or want to know more, complete this form or email us at hello@magboo.co</p></div>
             <div><input type="text" placeholder="Your Email"/></div>
             <div><button>Sign up</button></div>
         </div>
