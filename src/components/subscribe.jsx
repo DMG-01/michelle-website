bb873135cb1d2d12ea3203aba901cba2e57bbba6
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2025-06-07 21:26:17 +0100

    fixed contact linking

--- src/components/subscribe.jsx
+++ src/components/subscribe.jsx
@@ -1,15 +1,22 @@
 import react from "react"
+import { Link } from "react-router-dom";
+
 
 function Subscribe() {
-
-    return(
+    return (
         <div className="subscribe">
             <div><h2>CONTACT</h2></div>
-            <div><p>The process is super fast and super simple. If you're interested or want to know more, complete this form or email us at hello@magboo.co</p></div>
-            {/*<div><input type="text" placeholder="Your Email"/></div>*/}
-            <div><button><a href="./contact">Send Email</a></button></div>
-    
+            <div>
+                <p>The process is super fast and super simple. If you're interested or want to know more, complete this form or email us at hello@magboo.co</p>
+            </div>
+
+            <div>
+                <Link to="/contact">
+                    <button>Send Email</button>
+                </Link>
+            </div>
         </div>
-    )
+    );
 }
+
 export default Subscribe
